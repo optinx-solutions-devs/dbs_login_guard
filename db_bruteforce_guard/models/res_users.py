17bc16978bd86e4b93689b28cfb8760757a7a725
pk-co <prasacode.com>
2026-04-26 19:22:58 +0000

    [INIT] addon

---
+++ db_bruteforce_guard/models/res_users.py
@@ -0,0 +1,29 @@
+from odoo import SUPERUSER_ID, api, models
+from odoo.exceptions import AccessDenied
+
+
+class ResUsers(models.Model):
+    _inherit = "res.users"
+
+    @classmethod
+    def _login(cls, db, login, password, user_agent_env):
+        if not password:
+            raise AccessDenied()
+
+        with cls.pool.cursor() as cr:
+            env = api.Environment(cr, SUPERUSER_ID, {})
+            guard = env["db.guard.service"].sudo()
+            guard._ensure_not_blocked(login=login)
+
+        try:
+            auth_info = super()._login(db, login, password, user_agent_env)
+        except AccessDenied:
+            with cls.pool.cursor() as cr:
+                env = api.Environment(cr, SUPERUSER_ID, {})
+                env["db.guard.service"].sudo()._record_failure(login=login, database=db)
+            raise
+
+        with cls.pool.cursor() as cr:
+            env = api.Environment(cr, SUPERUSER_ID, {})
+            env["db.guard.service"].sudo()._record_success(login=login, database=db)
+        return auth_info
